62a548c2958a1d80b54d054e6ee127292d0b4543
mrmkultimate <kruse230@umn.edu>
2015-01-24 06:17:30 -0600

    head movement added

---
+++ Project/Assets/Generator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+	public GameObject SaltPrefab;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Instantiate(SaltPrefab);
+	}
+}
---
+++ Project/Assets/HeadFollowing.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadFollowing : MonoBehaviour {
+	public Transform headTransform;
+	float timePassed=0.0f;
+	bool warp=false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timePassed+=Time.deltaTime;
+		if(timePassed>2.0f){
+			warp = true;
+		}
+		if(warp)
+			transform.localPosition = headTransform.localPosition+new Vector3(-0.9203f,0,0);
+	}
+}
