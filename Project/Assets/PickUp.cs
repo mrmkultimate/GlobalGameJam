39c411c79494490895cf084b67dc27e241a99e22
mrmkultimate <kruse230@umn.edu>
2015-01-24 05:43:57 -0600

    Bumpers to pick up and fixed some bugs

--- Project/Assets/PickUp.cs
+++ Project/Assets/PickUp.cs
@@ -2,8 +2,20 @@
 using System.Collections;
 
 public class PickUp : MonoBehaviour {
-	bool selected = false;
-	bool pickedUp = false;
+	bool selectedRight = false;
+	bool pickedUpRight = false;
+	bool selectedLeft = false;
+	bool pickedUpLeft = false;
+	bool inJar = true;
+	bool inMortar = false;
+	bool inBurner = false;
+	bool inDropBox = false;
+	bool inPelican = false;
+
+	float burnedAmount = 0.0f;
+	bool burnt = false;
+	float crushedAmount = 0.0f;
+	bool crushed = false;
 	public Rigidbody rigidBody;
 	public Material mat;
 	public Texture tex1;
@@ -11,49 +23,71 @@ public class PickUp : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) {
 		Debug.Log("collided");
-		if(other.tag == "Player")
-			selected = true;
+		if(other.tag == "PlayerLeft")
+			selectedLeft = true;
+		if(other.tag == "PlayerRight")
+			selectedRight = true;
 	}
 	void OnTriggerStay(Collider other){
 
-		if(other.tag == "Player"){
-			if(Input.GetButton ("Fire1")){
-				pickedUp = true;
+		if(other.tag == "PlayerRight"){
+			if(Input.GetButton ("Fire1")&&(!pickedUpLeft)){
+				pickedUpRight = true;
 				transform.position = other.transform.position;
 			}else{
-				pickedUp = false;
+				pickedUpRight = false;
+			}
+		}
+		if(other.tag == "PlayerLeft"){
+			if(Input.GetButton ("Fire2")&&(!pickedUpRight)){
+				pickedUpLeft = true;
+				transform.position = other.transform.position;
+			}else{
+				pickedUpLeft = false;
 			}
 		}
 	}
 	void OnTriggerExit(Collider other){
-		if(other.tag == "Player"){
-			if(Input.GetButton ("Fire1")){
-				pickedUp = true;
+		if(other.tag == "PlayerRight"){
+			if(Input.GetButton ("Fire1")&&(!pickedUpLeft)){
+				pickedUpRight = true;
 				transform.position = other.transform.position;
 			}else{
-			selected = false;
-			pickedUp = false;
+			selectedRight = false;
+			pickedUpRight = false;
 			}
 		}
-
+		if(other.tag == "PlayerLeft"){
+			if(Input.GetButton ("Fire2")&&(!pickedUpRight)){
+				pickedUpLeft = true;
+				transform.position = other.transform.position;
+			}else{
+				selectedLeft = false;
+				pickedUpLeft = false;
+			}
+		}
 	}
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
-		if(pickedUp){
+		if(pickedUpLeft||pickedUpRight){
 			rigidBody.useGravity = false;
+			transform.rotation.eulerAngles.Set (0.0f,0.0f,0.0f);
+			rigidBody.freezeRotation = true;
 
 		}else{
 			rigidBody.useGravity = true;
+			rigidBody.freezeRotation = false;
+
 		}
 
-		if(selected){
+		if(selectedRight||selectedLeft){
 			mat.color = Color.red;
 		}
 		else{
