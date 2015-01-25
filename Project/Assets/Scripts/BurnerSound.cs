1a4cb0ead3643c6c40e9ed4f43acd6a4f924c910
mrmkultimate <kruse230@umn.edu>
2015-01-25 16:57:36 -0600

    Final Commit

--- Project/Assets/Scripts/BurnerSound.cs
+++ Project/Assets/Scripts/BurnerSound.cs
@@ -6,10 +6,14 @@ public class BurnerSound : MonoBehaviour {
 
 
 	public AudioSource Sizzle;
+	public GameObject particle;
+	Object temp;
 
 	void OnTriggerStay(Collider other){
 		if(other.tag == "Flower"||other.tag == "Salt"||other.tag == "Humous"||other.tag == "Eyeball"){
 			if(!Sizzle.isPlaying){
+				temp = Instantiate(particle);
+				Destroy(temp, 5.0f);
 				Sizzle.Play ();
 			}
 		}
